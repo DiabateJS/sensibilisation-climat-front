e5e26736b36c1bec10b18ab5adeb07354a042ae8
ET34471 <jean.diabate@bred.fr>
2023-09-26 18:00:06 +0200

    Mise en place des premiers composants.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuizComponent } from './quiz/quiz.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'quiz',
+    component: QuizComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
